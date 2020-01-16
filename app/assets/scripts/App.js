936356cee86691a647056d3260f6af5e782bdfc6
kpeleshko <kate200ka@gmail.com>
2020-01-16 10:50:36 +0200

    Completed sticky header.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
-import "../styles/styles.css";
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
+import "../styles/styles.css"
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
+let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 85)
 new RevealOnScroll(document.querySelectorAll('.testimonials'), 75)
 
